f5e533174cc5ae61df1eb2bea63b03e3e2166d04
Emilya <emilya.okmen@student.ehb.be>
2026-02-12 13:13:50 +0100

    size-ball

--- 01-interactive-bounce/main.js
+++ 01-interactive-bounce/main.js
@@ -2,6 +2,14 @@ let canvasElement = document.querySelector("#canvas");
 
 //1. Add variables needed for animating the ball
 
+let speed = 10;
+directionx = 1;
+let colorVal = "red";
+let size = 50;
+
+let x = 300;
+let y = 300;
+
 //2. Add variables (size, speed, colorVal) for controlling the sketch
 
 
@@ -19,10 +27,26 @@ function draw() {
     // 3.2 Bounce logic: check the edges
 
     // 3.3 Draw
+    fill(colorVal);
+    circle(x, y, size);
+
 }
 
 // --- DOM Interaction ---
 
 // 4. Select the input elements
 
+let sizeInput = document.querySelector("#size-input");
+
+console.log(sizeInput.value); //maakt value ball 50
+
+console.log(Number(sizeInput.value)); //size of the ball kan veranderd worden
+
+
 // 5. Add Event Listeners; listen to the "input" event for real-time updates while dragging
+
+sizeInput.addEventListener("input", function () {
+
+    size = Number(sizeInput.value); //veranderd nu de size van de bal met de slider
+
+});
